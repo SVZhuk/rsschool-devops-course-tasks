a01ad4817da0351ea0cf2c33edbff84e834ee3c9
SVZhuk <15237898+SVZhuk@users.noreply.github.com>
2025-06-14 22:07:31 +0300

    fix terraform formatting

--- main.tf
+++ main.tf
@@ -76,20 +76,20 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
     effect = "Allow"
 
     principals {
-      type = "Federated"
+      type        = "Federated"
       identifiers = [aws_iam_openid_connect_provider.github_actions.arn]
     }
 
     actions = ["sts:AssumeRoleWithWebIdentity"]
 
     condition {
-      test = "StringEquals"
+      test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:aud"
-      values = ["sts.amazonaws.com"]
+      values   = ["sts.amazonaws.com"]
     }
 
     condition {
-      test = "StringLike"
+      test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
       values = [
         "repo:${var.github_repository}:*"
